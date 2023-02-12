f593f1c6860eb50eb633cc1aeff615469a729bc3
fowik <ilarimsa937@gmail.com>
2023-02-12 23:13:02 +0200

    added some style for page

---
+++ assets/views/nav-bar.php
@@ -0,0 +1,15 @@
+<body>
+    <nav>
+        <div class="nav-bar">
+            <a href="./"><h1>Danfort</h1></a>
+            <div class="links">
+                <a href="">Sākumlapa</a>
+                <a href="">Par mums</a>
+                <a href="">Partneri</a>
+                <a href="">Produkcijas katalogs</a>
+                <a href="">Kontakti</a>
+                <a href="">Katalogs</a>
+            </div>
+        </div>
+    </nav>
+</body>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- LINKS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/nav-bar.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- LINKS -->
+    <title>Index</title>
+</head>
+<body>
+    <div class="wrapper">
+
+    <?php include 'assets/views/nav-bar.php'; ?>
+
+        <div class="main">
+            
+        </div>
+
+    <?php include 'assets/views/footer.php'; ?>
+    
+    </div>
+</body>
+</html>
